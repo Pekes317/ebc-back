f8672db0badd4b2bf8388531da23e6449c5075f5
Pekes317 <El_Dante565@yahoo.com>
2016-08-11 23:01:12 -0500

    Added MD Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,48 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MdCoreModule } from '@angular2-material/core';
-import { MdButtonModule } from '@angular2-material/button';
-import { MdButtonToggleModule } from '@angular2-material/button-toggle';
-import { MdCardModule } from '@angular2-material/card';
-import { MdCheckboxModule } from '@angular2-material/checkbox';
-import { MdGridListModule } from '@angular2-material/grid-list';
-import { MdIconModule } from '@angular2-material/icon';
-import { MdInputModule } from '@angular2-material/input';
-import { MdListModule } from '@angular2-material/list';
-import { MdMenuModule } from '@angular2-material/menu';
-import { MdProgressBarModule } from '@angular2-material/progress-bar';
-import { MdProgressCircleModule } from '@angular2-material/progress-circle';
-import { MdRadioModule } from '@angular2-material/radio';
-import { MdSidenavModule } from '@angular2-material/sidenav';
-import { MdSlideToggleModule } from '@angular2-material/slide-toggle';
-import { MdSliderModule } from '@angular2-material/slider';
-import { MdTabsModule } from '@angular2-material/tabs';
-import { MdToolbarModule } from '@angular2-material/toolbar';
-import { MdTooltipModule } from '@angular2-material/tooltip';
 
+import { MaterialModule } from './md.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  imports: [ BrowserModule,
-    MdCoreModule,
-    MdButtonModule,
-    MdButtonToggleModule,
-    MdCardModule,
-    MdCheckboxModule,
-    MdIconModule,
-    MdGridListModule,
-    MdInputModule,
-    MdListModule,
-    MdMenuModule,
-    MdProgressBarModule,
-    MdProgressCircleModule,
-    MdRadioModule,
-    MdSidenavModule,
-    MdSliderModule,
-    MdSlideToggleModule,
-    MdTabsModule,
-    MdToolbarModule,
-    MdTooltipModule ],
+  imports: [ BrowserModule, MaterialModule ],
   declarations: [ AppComponent ],
   bootstrap: [ AppComponent ]
 })
