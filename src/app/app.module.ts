f495b0af342cfc1ef4a2226b5f0370c496536c23
Pekes317 <El_Dante565@yahoo.com>
2016-08-26 23:40:16 -0500

    Setting up app landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { MaterialModule } from './md.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home';
 
 @NgModule({
   imports: [ BrowserModule, MaterialModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, HomeComponent ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
