15e9ad4c03f8c4d5aaa3424f856fa4a5b97d4947
Pekes317 <El_Dante565@yahoo.com>
2018-05-02 22:03:46 -0500

    move json

--- server/modules/img-upload/img-upload.service.ts
+++ server/modules/img-upload/img-upload.service.ts
@@ -1,19 +1,16 @@
 import { Component } from '@nestjs/common';
 import * as cloudinary from 'cloudinary';
 
+const ebcCloud = require('./cloudinary.json');
+
 @Component()
 export class ImgUploadService {
-	ebcCloud = {
-		cloud_name: 'ebccloud',
-		api_key: '383232894147658',
-		api_secret: 'JfhdN-ova6QoO0NStnXGlpaD3II'
-	};
 
 	constructor() { }
 
 	async addSign(upload: any) {
 		let newImg = {};
-		cloudinary.config(this.ebcCloud);
+		cloudinary.config(ebcCloud);
 		await cloudinary.v2.uploader.upload(upload.img, upload.opts, (err, cloud) => {
 			newImg = cloud;
 			if (err) {
